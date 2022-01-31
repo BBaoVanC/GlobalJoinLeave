61d0fc9c989d52fc7bea1c3e8c17f1d07e01f0a3
BBaoVanC <bbaovanc@bbaovanc.com>
2022-01-30 18:11:26 -0600

    Bump version to 1.0.2

--- src/main/java/best/boba/globaljoinleave/GlobalJoinLeave.java
+++ src/main/java/best/boba/globaljoinleave/GlobalJoinLeave.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 
 @Plugin(id = "globaljoinleave",
         name = "GlobalJoinLeave",
-        version = "1.0.1",
+        version = "1.0.2",
         url = "https://github.com/bobacraft/GlobalJoinLeave",
         authors = {"bbaovanc"},
         description = "Global join/leave messages for the Velocity proxy"
